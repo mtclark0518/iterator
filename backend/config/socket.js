ed9159290d4313b174970ae30c84ef553135ac8d
Tyler Clark <mtclark0518@gmail.com>
2017-11-25 19:24:32 -0700

    adds some comments to serverside code

---
+++ backend/config/socket.js
@@ -0,0 +1,69 @@
+
+
+
+const log = (stuff) => console.log(stuff)
+const db = require('../models')
+
+module.exports = (io) => {
+io.on('connection', (socket) => {
+  db.models.container.findOne({
+    where: {id : 1}})
+    .then(response => {
+      let users = parseInt(response.dataValues.activeUsers)      
+      log('a user connected');
+      users++
+      response.updateAttributes({ 
+        activeUsers: users })
+        .then(response => {
+          let updatedUsers = response.dataValues.activeUsers
+          io.sockets.emit('update users', {
+            users: updatedUsers
+          })
+          let number = response.dataValues.number
+          io.sockets.emit('welcome', {
+            number: number,
+            name: 'iterator',
+          })
+
+        })
+    });
+
+  socket.on('iterate', data => {
+    let changeBy = parseInt(data)
+    db.models.container.findOne({
+      where: {id : 1}
+    })
+    .then( number => {
+      let numTo = parseInt(number.dataValues.number);
+      changeBy = numTo + changeBy;
+      number.updateAttributes({
+        number: changeBy
+      })
+      .then( newNumber => {
+        let newNum = newNumber.dataValues.number;
+        io.sockets.emit('iterated', newNum)
+      });
+    });
+  });
+
+  socket.on('disconnect', () => {
+    db.models.container.findOne({
+      where: {id : 1}
+    })
+    .then(response => {
+      let users = response.dataValues.activeUsers
+      log('a user dipped')
+      users--
+      response.updateAttributes({
+        activeUsers: users 
+      })
+      .then(response => {
+        let updatedUsers = response.dataValues.activeUsers
+        io.sockets.emit('update users', {
+          users: updatedUsers 
+        });
+      });
+    });
+  });
+});
+};
--- server.js
+++ server.js
@@ -1,110 +1,45 @@
 const express = require('express')
+const app = express()
 const http = require('http')
-const app = express();
 const server = http.createServer(app)
-const PORT = process.env.PORT || 1979
 
+//utility function for laziness
+const log = (stuff) => console.log(stuff)
 
-
-const {Client} = require('pg')
-const client = new Client({ 
-  connectionString: process.env.DATABASE_URL,
-  ssl: true 
-});
-client.connect( (err) => {
-	if (err) { log('error yo: ', err) } else { log('connected to db') }
-});
-
-
-
+//for passing form data
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
-
-const path = require('path')
+//backend routing and views
 const routes = require('./backend/config/routes')
-
+const path = require('path')
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/', routes)
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
+//used to connect to heroku postgres 
+const {Client} = require('pg')
+const client = new Client({ 
+  connectionString: process.env.DATABASE_URL,
+  ssl: true 
+  })
+  .connect((err) => {
+    if (err) { 
+      log('error yo: ', err)
+    } else { 
+      log('connected to db')
+    }
+  });
 
-server.listen(PORT, () => log('Giggity goo on port: ' + PORT));
-
-
-
-
-const log = (stuff) => console.log(stuff)
-
-
-const db = require('./backend/models')
-
-
+//socketio instance
 const socketio = require('socket.io')
 const io = socketio(server)
+//imports module and passes the socketio connection
+require('./backend/config/socket')(io)
 
-io.on('connection', (socket) => {
-  db.models.container.findOne({
-    where: {id : 1}})
-    .then(response => {
-      let users = parseInt(response.dataValues.activeUsers)      
-      log('a user connected');
-      users++
-      response.updateAttributes({ 
-        activeUsers: users })
-        .then(response => {
-          let updatedUsers = response.dataValues.activeUsers
-          io.sockets.emit('update users', {
-            users: updatedUsers
-          })
-          let number = response.dataValues.number
-          io.sockets.emit('welcome', {
-            number: number,
-            name: 'iterator',
-          })
-
-        })
-    });
-
-  socket.on('iterate', data => {
-    let changeBy = parseInt(data)
-    db.models.container.findOne({
-      where: {id : 1}
-    })
-    .then( number => {
-      let numTo = parseInt(number.dataValues.number);
-      changeBy = numTo + changeBy;
-      number.updateAttributes({
-        number: changeBy
-      })
-      .then( newNumber => {
-        let newNum = newNumber.dataValues.number;
-        io.sockets.emit('iterated', newNum)
-      });
-    });
-  });
-
-  socket.on('disconnect', () => {
-    db.models.container.findOne({
-      where: {id : 1}
-    })
-    .then(response => {
-      let users = response.dataValues.activeUsers
-      log('a user dipped')
-      users--
-      response.updateAttributes({
-        activeUsers: users 
-      })
-      .then(response => {
-        let updatedUsers = response.dataValues.activeUsers
-        io.sockets.emit('update users', {
-          users: updatedUsers 
-        })
-      })
-    });
-  });
-});
-
+//connects to the server
+const PORT = process.env.PORT || 1979
+server.listen(PORT, () => log('Giggity goo on port: ' + PORT));
