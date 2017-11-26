3ec2e43f0d8c5fa9979a307e99cc91f8563256e7
Tyler Clark <mtclark0518@gmail.com>
2017-11-25 21:42:45 -0700

    test

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,7 @@
 const localDB = require('../../local.env.js')
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(process.env.DATABASE_URL || localDB.localDB )
+const sequelize = console.log(localDB)
+new Sequelize(process.env.DATABASE_URL || localDB )
 const container = sequelize.import('./container')
 const user = sequelize.import('./user')
 
